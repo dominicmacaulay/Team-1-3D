4376aee42fe7863c3fb9c1f25dde6f356ba80edc
dominicmacaulay <dominic.62502@gmail.com>
2023-07-11 19:32:33 -0400

    Panel change, acid and laser interaction manager

--- Team #1 [3D]/Assets/Scripts/Player/CollisionsManager.cs
+++ Team #1 [3D]/Assets/Scripts/Player/CollisionsManager.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class CollisionsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isAlive = true;
+    public float gracePeriod;
+    public float panelTime;
+
+    bool isTouchingAcid = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "acid")
+        {
+            isTouchingAcid = true;
+            StartCoroutine(AcidEffect());
+            StartCoroutine(Death());
+        }
+
+        if (other.gameObject.tag == "lasers")
+        {
+            isAlive = false;            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.gameObject.tag == "acid")
+        {
+            isTouchingAcid = false;
+            StartCoroutine(AcidRegen());
+            StopCoroutine(Death());
+        }
+    }
+
+    IEnumerator AcidEffect()
+    {
+        while (isTouchingAcid == true)
+        {
+            yield return new WaitForSeconds(panelTime);
+            Debug.Log("acid");
+            //panel stuff
+        }
+        yield break;
+    }
+
+    IEnumerator AcidRegen()
+    {
+        while (isTouchingAcid == false)
+        {
+            yield return new WaitForSeconds(panelTime);
+            //panel stuff
+            Debug.Log("regen");
+        }
+        yield break;
+    }
+
+    IEnumerator Death()
+    {
+        yield return new WaitForSeconds(gracePeriod);
+
+        isAlive = false;
+        Debug.Log(isAlive);
     }
 }
