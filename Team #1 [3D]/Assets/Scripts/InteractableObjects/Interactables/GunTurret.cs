edc409b4f137f0f18dc268a815a6de002d1b03d8
dominicmacaulay <dominic.62502@gmail.com>
2023-07-11 21:54:14 -0400

    Attempts at Turret AI

--- Team #1 [3D]/Assets/Scripts/InteractableObjects/Interactables/GunTurret.cs
+++ Team #1 [3D]/Assets/Scripts/InteractableObjects/Interactables/GunTurret.cs
@@ -6,11 +6,35 @@ public class GunTurret : MonoBehaviour
 {
     public Animator anim;
 
+    public Transform player;
+    public Transform turretHead;
+    float distance;
+    public float maxDistance;
+
+
     // Start is called before the first frame update
     void Start()
     {
         //anim.SetBool("isShooting", true);
         Debug.Log("shooting");
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance(player.position, transform.position);
+
+        //if (distance <= maxDistance )
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            turretHead.LookAt(player);
+        }
     }
 
     public void DeactivateTurret()
